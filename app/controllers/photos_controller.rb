b2c1da028ef0303b29db0c8b8adbfee05523ddce
dylanthompson1 <dylanthompson@uchicago.edu>
2022-02-22 01:36:13 +0000

    delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,4 +10,31 @@ def index
 
 end
 
+def show
+
+url_id = params.fetch("path_id")
+
+matching_photos = Photo.where({ :id => url_id })
+
+@the_photo = matching_photos.at(0)
+
+render({ :template => "photo_templates/show.html.erb" })
+
+end
+
+def baii
+
+  the_id = params.fetch("toast_id")
+
+  matching_photos = Photo.where({ :id => the_id})
+  
+  the_photo = matching_photos.at(0)
+
+  the_photo.destroy
+
+  redirect_to("/photos")
+
+  #render({ :template => "photo_templates/baii.html.erb" })
+
+end
 end
